4dca27792838307d4894549598616fd3c98d65df
kawaii-Code <nia.personal.0@gmail.com>
2024-12-11 18:06:07 +0300

    Initial commit 🚀

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Calendar } from 'vanilla-calendar-pro'
+
+const calendar = new Calendar('#calendar');
+calendar.init();
+
---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "vanilla-calendar-pro" # @3.0.1
