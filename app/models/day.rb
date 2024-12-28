ffee41783296fb93b6c2ca785a4c95d65856aba7
VIPigeon <slv.bta@gmail.com>
2024-12-28 20:10:03 +0300

    mega test for day

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,13 @@ class Day < ApplicationRecord
   belongs_to :user
   validates :date, presence: true, uniqueness: { scope: :user }
   validates :rating, presence: true, inclusion: { in: %w[good bad] }
+  validate :date_cannot_be_in_the_future
+
+  private
+
+  def date_cannot_be_in_the_future
+    if date.present? && date > Date.today
+      errors.add(:date, "day cannot be in the future 😡")
+    end
+  end
 end
--- test/models/day_test.rb
+++ test/models/day_test.rb
@@ -1,7 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class DayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(name: "Test User") # Предполагается, что у вас есть модель User
+    @day = Day.new(date: Date.today, rating: 'good', user: @user)
+  end
+
+  test "should be valid" do
+    assert @day.valid?
+  end
+
+  test "date should be present" do
+    @day.date = nil
+    assert_not @day.valid?
+  end
+
+  test "rating should be present" do
+    @day.rating = nil
+    assert_not @day.valid?
+  end
+
+  test "rating should be good or bad" do
+    @day.rating = 'good'
+    assert @day.valid?
+    @day.rating = 'bad'
+    assert @day.valid?
+    @day.rating = '12345'
+    assert_not @day.valid?
+    @day.rating = 'normal'
+    assert_not @day.valid?
+    @day.rating = 'bd'
+    assert_not @day.valid?
+    @day.rating = '⛅'
+    assert_not @day.valid?
+  end
+
+  test "date should be unique per user" do
+    @day.save
+    duplicate_day = @day.dup
+    assert_not duplicate_day.valid?
+  end
+
+  test "date cannot be in the future" do
+    @day.date = Date.tomorrow
+    assert_not @day.valid?
+    assert_includes @day.errors[:date], "day cannot be in the future 😡"
+  end
 end
