3cb53dd83ae0b85fbd29e1cf4c200bf71524281d
UnicornTsar <rodionoff.eg@yandex.ru>
2024-12-28 21:58:20 +0300

    Делаю таблицу достижений

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :days
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   def self.current
     User.find(Current.session.user_id)
+  end
+  
+  def get_count_good_days
+    days.where('rating = ?', "good").count
   end
 end
