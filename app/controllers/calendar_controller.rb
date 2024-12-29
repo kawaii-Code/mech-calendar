d540b2c4504427d761e5289cf310dbb927982967
Linux Torbolts <114951816+MakimaGorky@users.noreply.github.com>
2024-12-29 12:22:26 +0300

    haha rip

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,7 +2,7 @@ class CalendarController < ApplicationController
 
   def data
     # Извлечение данных из базы данных
-    events = Event.select(:date, :rating).as_json
+    events = Day.select(:date, :rating).as_json
     render json: events
   end
 
